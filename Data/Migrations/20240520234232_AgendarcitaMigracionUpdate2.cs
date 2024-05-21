35a3dabad7433f0e6375c02f2b5ca5920eb1ca1f
dmcnero123 <jair_duran@usmp.pe>
2024-05-20 19:12:08 -0500

    agregando estilos y paginas para las citas y el garantia

---
+++ Data/Migrations/20240520234232_AgendarcitaMigracionUpdate2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NebulaCars.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgendarcitaMigracionUpdate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "t_Citas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Telefono = table.Column<string>(type: "text", nullable: false),
+                    Ciudad = table.Column<string>(type: "text", nullable: false),
+                    TipoDocumento = table.Column<string>(type: "text", nullable: false),
+                    NumeroDocumento = table.Column<string>(type: "text", nullable: false),
+                    Marca = table.Column<string>(type: "text", nullable: false),
+                    Modelo = table.Column<string>(type: "text", nullable: false),
+                    Kilometraje = table.Column<int>(type: "integer", nullable: false),
+                    Placa = table.Column<string>(type: "text", nullable: false),
+                    Mensaje = table.Column<string>(type: "text", nullable: true),
+                    FechaCita = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HoraCita = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Citas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_Citas");
+        }
+    }
+}
